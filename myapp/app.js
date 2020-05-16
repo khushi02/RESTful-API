b0e90b4cfdacdbafa55e5b8d084c02379a5a770b
khushi02 <k9wadhwa@gmail.com>
2020-05-16 13:03:14 -0700

    update

--- myapp/app.js
+++ myapp/app.js
@@ -1,4 +1,6 @@
 var express = require('express');
 var app = express();
 var db = require('./db');
+var userController = require('./user/userController');
+app.use('/users', userController);
 module.exports = app;
--- myapp/user/userController.js
+++ myapp/user/userController.js
@@ -5,3 +5,25 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 var User = require('./user');
 module.exports = router;
+
+// Create new user
+router.post('/', function (req, res) {
+    User.create({
+            name : req.body.name,
+            email : req.body.email,
+            password : req.body.password
+        }, 
+        function (err, user) {
+            if (err) return res.status(500).send("There was a problem adding the information to the database.");
+            res.status(200).send(user);
+        });
+});
+
+// Return all users in database
+router.get('/', function (req, res) {
+    User.find({}, function (err, users) {
+        if (err) return res.status(500).send("There was a problem finding the users.");
+        res.status(200).send(users);
+    });
+});
+module.exports = router;
