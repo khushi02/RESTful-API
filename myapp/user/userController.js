f3433118e5d337a48c42f2dfa1b5c26c1678ca87
khushi02 <k9wadhwa@gmail.com>
2020-05-16 14:55:49 -0700

    Update userController.js

--- myapp/user/userController.js
+++ myapp/user/userController.js
@@ -27,3 +27,28 @@ router.get('/', function (req, res) {
     });
 });
 module.exports = router;
+
+// Return single user in database
+router.get('/:id', function (req, res) {
+    User.findById(req.params.id, function (err, user) {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+        res.status(200).send(user);
+    });
+});
+
+//Update single user in database
+router.put('/:id', function (req, res) {
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
+        if (err) return res.status(500).send("There was a problem updating the user.");
+        res.status(200).send(user);
+    });
+});	
+
+// Delete single user in database
+router.delete('/:id', function (req, res) {
+    User.findByIdAndRemove(req.params.id, function (err, user) {
+        if (err) return res.status(500).send("There was a problem deleting the user.");
+        res.status(200).send("User "+ user.name +" was deleted.");
+    });
+});
